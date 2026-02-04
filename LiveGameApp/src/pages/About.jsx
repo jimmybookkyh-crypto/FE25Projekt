1f4c2ef37e4d3337f1398c7535ad9b8fb1339b47
JimmyBookKyh <jimmybookkyh@gmail.com>
2026-02-04 10:48:43 +0100

    about update

--- LiveGameApp/src/pages/About.jsx
+++ LiveGameApp/src/pages/About.jsx
@@ -2,22 +2,25 @@ import ContentBox from "../layout/contentBox/ContentBox";
 
 function About(){
     return (
-        <ContentBox title="About Live Game App">
-            <p>School project for FE2025 JS2 course.</p>
-            <p> Written in Visual Studio Code.</p>
-            <p>Visual Studio Code:<br/>
-            <a href="https://www.mmobomb.com/api" target="_blank" rel="noopener noreferrer" >
+        <ContentBox title="About Live Gaming App">
+            <a href="https://github.com/jimmybookkyh-crypto/FE25Projekt" target="_blank" rel="noopener noreferrer" >
+            Github Link</a>
+            <p>School project for FE2025 JS2 course.<br/> Created by Jimmy Book.<br/>
+            <a href="https://github.com/jimmybookkyh-crypto" target="_blank" rel="noopener noreferrer" >
+            Git repository
+            </a>
+            </p>
+            <p> Written in Visual Studio Code.<br/> Visual Studio Code:<br/>
+            <a href="https://code.visualstudio.com/" target="_blank" rel="noopener noreferrer" >
             https://code.visualstudio.com/
             </a>
             </p>
-            <p>Created using React and React Route.</p>
-            <p>React Library:<br/>
+            <p>Created using React and React Route.<br/> React Library:<br/>
             <a href="https://www.mmobomb.com/api" target="_blank" rel="noopener noreferrer" >
             https://react.dev/
             </a>
             </p>
-            <p>
-                API database used:<br/>
+            <p>API database used:<br/>
             <a href="https://www.mmobomb.com/api" target="_blank" rel="noopener noreferrer" >
             https://www.mmobomb.com/api
             </a>
